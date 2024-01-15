df689f6933bd36a7ab7f672540e1cfccdcaaf0c6
custoward <mt0908@naver.com>
2024-01-15 18:04:19 +0900

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect, useRef } from 'react';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
 
+import Canvas from "./canvas";
+
+
+
 class App extends Component {
   render() {
-    return ( <h1>Hello World!</h1> );
+    return (
+      <div>
+        <div className="title">Custoward</div>
+        <div className="canvasProp"><Canvas></Canvas></div>
+      </div>
+    );
   }
 }
+
 
 export default App;
