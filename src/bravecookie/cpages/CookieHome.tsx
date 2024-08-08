b57281b5874a7aa1f18c139d403802da5bf09ed7
custoward <mt0908@naver.com>
2024-08-08 22:05:25 +0900

    ogimage

--- src/bravecookie/cpages/CookieHome.tsx
+++ src/bravecookie/cpages/CookieHome.tsx
@@ -24,6 +24,8 @@ const CookieHome: React.FC = () => {
         <title>용감한 쿠키 2024</title>
         <meta property="og:title" content="용감한 쿠키 2024" />
         <meta property="og:description" content="2024 정기 공연, 여름." />
+        <meta property="og:site_name" content="davi-davi.com/bravecookie" />
+        <meta property="og:image" content="/ogimage.ipg" />
         <link rel="icon" href="/faviconTwo.ico?v=2" sizes="16x16" />
       </Helmet>
       <div className={`loading-screen ${fadeOut ? 'fade-out' : ''}`}>
--- src/bravecookie/cpages/SetListIntroPage.tsx
+++ src/bravecookie/cpages/SetListIntroPage.tsx
@@ -22,6 +22,7 @@ const SetListIntroPage: React.FC = () => {
         <meta property="og:description" content="2024 정기 공연, 여름." />
         <link rel="icon" href="/faviconTwo.ico?v=2" sizes="16x16" />
         <meta property="og:site_name" content="davi-davi.com/bravecookie" />
+        <meta property="og:image" content="/ogimage.ipg" />
       </Helmet>
       <div className="half-screen day" onClick={handleLeftClick}>
         <div className="intro">
