b685d534b49a36fbbb1cad401393f7174e42d707
custoward <mt0908@naver.com>
2024-08-08 18:55:08 +0900

    gogo

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,11 @@ class App extends Component {
           <Route exact path="/">
             <Helmet>
               <link rel="icon" href="/favicon.ico" />
+              <meta
+                name="Davi-Davi"
+                content="Davi-Davi Design Team Portfolio"
+              />
+              <title>DAVI_DAVI</title>
             </Helmet>
             <HomePage />
           </Route>
@@ -26,7 +31,7 @@ class App extends Component {
             <Helmet>
               <title>Brave Cookie</title>
               <meta property="og:title" content="용감한쿠키 2024 여름 정기공연" />
-            <meta property="og:description" content="여름의 낮, 여름의 밤" />
+              <meta property="og:description" content="여름의 낮, 여름의 밤" />
               <link rel="icon" href="/faviconTwo.ico" />
             </Helmet>
             <Switch>
