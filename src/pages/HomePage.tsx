e741a5884d7270f994830addee40b0d7ecd11668
custoward <mt0908@naver.com>
2024-08-08 21:53:12 +0900

    gogo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,15 +2,19 @@ import { Helmet } from 'react-helmet-async';
 import Layout from '../components/Layout/Layout';
 import Homecontainer from '../containers/Home/Homecontainer';
 
-const HomePage : React.FC = () => {
+const HomePage: React.FC = () => {
     return (
         <>
-        <div>
-        <div className="bg"></div>
-        <Layout>
-            <Homecontainer />
-        </Layout>
-        </div>
+            <Helmet>
+                <meta property="og:title" content="Davi-Davi" />
+                <meta property="og:description" content="Davi-Davi design studio, Since 2019" />
+            </Helmet>
+            <div>
+                <div className="bg"></div>
+                <Layout>
+                    <Homecontainer />
+                </Layout>
+            </div>
         </>
     )
 }
